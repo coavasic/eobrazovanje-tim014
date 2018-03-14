85f4186064ead96b839958d4747f02e53d2ac67a
Aleksandar Vasic <coavasic96@gmail.com>
2018-03-14 14:16:36 +0100

    Initial commit

---
+++ src/main/java/eobrazovanje/tim014/EObrazovanjeTim014Application.java
@@ -0,0 +1,12 @@
+package eobrazovanje.tim014;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EObrazovanjeTim014Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EObrazovanjeTim014Application.class, args);
+	}
+}
